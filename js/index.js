a7d1867ee91ed3dc0aa782d1da64d91c7e740c1d
Salma Hussein <salmahussienhassan>
2023-09-22 06:44:33 +0300

    upload

--- js/index.js
+++ js/index.js
@@ -1,51 +1,35 @@
-// dom elemnts
-const submitBtn=document.querySelector('.submit')
-const listBtn=document.querySelectorAll('li')
+const submitBtn = document.querySelector(".submit");
+const listBtn = document.querySelectorAll("li");
+const thanksBox = document.querySelector(".thanks");
+const cardBox = document.querySelector(".Card");
+const alertText = document.querySelector(".butn .alertText");
 
-const thanksBox=document.querySelector('.thanks')
-const cardBox=document.querySelector('.Card')
-console.log(cardBox)
+let rateNum = undefined;
 
-const Alert=document.querySelector('.butn .Alert')
-// variables
-let rateNum=undefined;
-
-// events
-
-for(let i=0;i<listBtn.length;i++){
-
-    listBtn[i].addEventListener('click',function(e){
-
-        rateNum=e.target.innerHTML;
+for (let i = 0; i < listBtn.length; i++) {
+    listBtn[i].addEventListener("click", function () {
+        rateNum = this.innerHTML;
         for (let j = 0; j < listBtn.length; j++) {
             if (j !== i) {
-              listBtn[j].classList.remove('selected-color');
+                listBtn[j].classList.remove("selected-color");
             }
         }
-console.log(e.target.innerHTML);
-    listBtn[i].classList.add('selected-color')
 
-
-    })
+        listBtn[i].classList.add("selected-color");
+    });
 }
 
-submitBtn.addEventListener('click',function(){
-    if(rateNum===undefined)
-    {Alert.classList.replace('d-none','d-block')
-        return
-    }
-    else{
-        Alert.classList.replace('d-bock','d-none')
-        cardBox.classList.add("d-none")
-      
-        thanksBox.classList.replace('d-none','d-block')
-        // thanksBox.classList.add("animate__animated","animate__fadeInRight")
+submitBtn.addEventListener("click", ()=> {
+    if (rateNum === undefined) {
+        alertText.classList.replace("d-none", "d-block");
+        return;
+    } else {
+        alertText.classList.replace("d-bock", "d-none");
+        cardBox.classList.add("d-none");
 
-       document.querySelector('.number').innerHTML=rateNum
-      
-    }
-    
-   
-})
+        thanksBox.classList.replace("d-none", "d-block");
+        thanksBox.classList.add("animate__animated", "animate__fadeInRight");
 
-// function
+        document.querySelector(".number").innerHTML = rateNum;
+    }
+});
